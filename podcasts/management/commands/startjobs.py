17f9348159fb01760978e86fdab7318ed9bad120
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-11-07 15:17:11 +0530

    Initial commit for the rss pull job command

---
+++ podcasts/management/commands/startjobs.py
@@ -0,0 +1,9 @@
+import feedparser
+from dateutil import parser
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **kwargs):
+        print("It works!")
