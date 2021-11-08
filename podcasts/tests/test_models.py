99b8c75eb8c45842cf9945d9da8cd0ce2bb3a438
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-11-08 18:23:16 +0530

    Moved script to a tests package

--- podcasts/tests/test_models.py
+++ podcasts/tests/test_models.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from django.utils import timezone
+from podcasts.models import Episode
 
-# Create your tests here.
+
+class EpisodeTests(TestCase):
+
+    def setUp(self):
+        self.episode = Episode.objects.create(
+            title="My awesome podcast episode",
+            description="look mom, I made it!",
+            pub_date=timezone.now(),
+            link="https://myawesomeshow.com",
+            image="https://image.myawesomeshow.com",
+            podcast_name="My Python Podcast",
+            guid="e7c1eb40-2e77-41e7-862b-a583eb19471a")
+
+    def test_episode_content(self):
+        self.assertEqual(self.episode.description, "look mom, I made it!")
+        self.assertEqual(self.episode.link, "https://myawesomeshow.com")
+        self.assertEqual(self.episode.guid, "e7c1eb40-2e77-41e7-862b-a583eb19471a")
+
+    def test_episode_str_representation(self):
+        self.assertEqual(str(self.episode), "My Python Podcast: My awesome podcast episode")
---
+++ podcasts/tests/test_views.py
@@ -0,0 +1,29 @@
+from django.test import TestCase
+from django.urls import reverse
+from django.utils import timezone
+from podcasts.models import Episode
+
+
+class HomePageTests(TestCase):
+
+    def setUp(self):
+        self.episode = Episode.objects.create(
+            title="My awesome podcast episode",
+            description="look mom, I made it!",
+            pub_date=timezone.now(),
+            link="https://myawesomeshow.com",
+            image="https://image.myawesomeshow.com",
+            podcast_name="My Python Podcast",
+            guid="e7c1eb40-2e77-41e7-862b-a583eb19471a")
+
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_renders_correct_template(self):
+        response = self.client.get(reverse('homepage'))
+        self.assertTemplateUsed(response, 'podcasts/homepage.html')
+
+    def test_home_page_list_contents(self):
+        response = self.client.get(reverse('homepage'))
+        self.assertContains(response, 'My awesome podcast episode')
