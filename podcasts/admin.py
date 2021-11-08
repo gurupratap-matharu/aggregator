50eb4293ee523869580ed9847d841e826175ac71
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-11-08 18:51:10 +0530

    Add search and filter fields for episode model

--- podcasts/admin.py
+++ podcasts/admin.py
@@ -5,4 +5,9 @@ from podcasts.models import Episode
 
 @admin.register(Episode)
 class EpisodeAdmin(admin.ModelAdmin):
-    list_display = ("podcast_name", "title", "pub_date")
+    list_display = ("podcast_name", "title", "pub_date", "active", "featured")
+    list_filter = ("active", "featured")
+    list_editable = ("active", "featured")
+    search_fields = ("podcast_name", "title")
+    date_hierarcy = "pub_date"
+    ordering = ("-pub_date",)
