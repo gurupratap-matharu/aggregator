0395e9adb7641f7d1e5c24d2c4443c152e50c737
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-11-08 19:10:53 +0530

    Added django-apscheduler to run jobs

--- podcasts/management/commands/startjobs.py
+++ podcasts/management/commands/startjobs.py
@@ -1,9 +1,106 @@
+import logging
+from datetime import timezone
+
 import feedparser
+from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.triggers.cron import CronTrigger
 from dateutil import parser
+from django.conf import settings
 from django.core.management.base import BaseCommand
+from django_apscheduler.jobstores import DjangoJobStore
+from django_apscheduler.models import DjangoJobExecution
+from podcasts.models import Episode
+
+logger = logging.getLogger(__name__)
+
+
+def save_new_episodes(feed):
+    """Saves new episodes to the database.
+    
+    Checks the episode GUID against the episodes currently stored in the database.
+    If not found, then a new `Episode` is added to the database.
+       
+    Args:
+        feed: requires a feedparser object
+    """
+
+    podcast_title = feed.channel.title
+    podcast_image = feed.channel.image["href"]
+
+    for item in feed.entries:
+        if not Episode.objects.filter(guid=item.guid).exists():
+            episode = Episode(
+                title=item.title,
+                description=item.description,
+                pub_date=parser.parse(item.published),
+                link=item.link,
+                image=podcast_image,
+                podcast_name=podcast_title,
+                guid=item.guid,
+            )
+            episode.save()
+
+
+def fetch_talkpython_episodes():
+    """Fetches new episodes from RSS for the The Real Python Podcast."""
+    print('fetching talkpython episodes...')
+
+    _feed = feedparser.parse("https://realpython.com/podcasts/rpp/feed")
+    save_new_episodes(_feed)
+
+
+def fetch_realpython_episodes():
+    print('fetching realpython episodes...')
+    _feed = feedparser.parse("https://realpython.com/podcasts/rpp/feed")
+    save_new_episodes(_feed)
+
+
+def delete_old_job_executions(max_age=604_800):
+    """Deletes all apscheduler job execution logs older than `max_age`."""
+    DjangoJobExecution.objects.delete_old_job_executions(max_age)
 
 
 class Command(BaseCommand):
+    help = "Runs apscheduler."
 
     def handle(self, *args, **kwargs):
-        print("It works!")
+        scheduler = BlockingScheduler(timezone=settings.TIME_ZONE)
+        scheduler.add_jobstore(DjangoJobStore(), "default")
+
+        scheduler.add_job(
+            fetch_realpython_episodes,
+            trigger="interval",
+            minutes=2,
+            id="The Real Python Podcast",
+            max_instances=1,
+            replace_existing=True)
+
+        logger.info("Added job: The Real Python Podcast.")
+
+        scheduler.add_job(
+            fetch_talkpython_episodes,
+            trigger="interval",
+            minutes=2,
+            id="Talk Python Feed",
+            max_instances=1,
+            replace_existing=True)
+
+        logger.info("Added job: Talk Python feed.")
+
+        scheduler.add_job(
+            delete_old_job_executions,
+            trigger=CronTrigger(day_of_week="mon", hour="00", minute="00"),
+            id="Delete Old Job Executions",
+            max_instances=1,
+            replace_existing=True,
+        )
+
+        logger.info("Added weekly job: Delete Old Job Executions.")
+
+        try:
+            logger.info("Starting scheduler...")
+            scheduler.start()
+        except KeyboardInterrupt:
+            logger.info("Stopping scheduler...")
+            scheduler.shutdown()
+            logger.info("Scheduler shut down successfully!")
