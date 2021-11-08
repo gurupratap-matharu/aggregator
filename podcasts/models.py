916048ca4b683c88a9ec7f04ccaba399a46ef9fd
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-11-08 18:59:05 +0530

    Add active manager to Episode model

--- podcasts/models.py
+++ podcasts/models.py
@@ -1,3 +1,37 @@
-from django.db import models
+"""Database models for the aggregator app"""
 
-# Create your models here.
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class ActiveManager(models.Manager):
+
+    def active(self):
+        return self.filter(active=True)
+
+    def featured(self):
+        return self.filter(featured=True)
+
+
+class Episode(models.Model):
+    """Data model for a Podcasts' episode"""
+
+    title = models.CharField(verbose_name=_("Title"), max_length=200)
+    description = models.TextField(verbose_name=_("Description"))
+    pub_date = models.DateTimeField(verbose_name=_("Publication Date"))
+    link = models.URLField(verbose_name=_("Url"))
+    image = models.URLField(verbose_name=_("Image"))
+    podcast_name = models.CharField(verbose_name=_("Podcast Name"), max_length=100)
+    guid = models.CharField(verbose_name=_("Guid"), max_length=50)
+    active = models.BooleanField(verbose_name=_("Active"), default=True)
+    featured = models.BooleanField(verbose_name=_("Featured"), default=False)
+
+    objects = ActiveManager()
+
+    class Meta:
+        verbose_name = _('Episode')
+        verbose_name_plural = _('Episodes')
+        ordering = ('-pub_date',)
+
+    def __str__(self) -> str:
+        return f"{self.podcast_name}: {self.title}"
