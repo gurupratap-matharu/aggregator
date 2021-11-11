9a0ffa861f1269f21e4a07bfab932b47ab9aacc3
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-11-11 11:31:50 +0530

    Remove old formatting

--- main/settings.py
+++ main/settings.py
@@ -39,12 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
 
-     # 3rd party
+    # 3rd party
     'django_apscheduler',
     'django_extensions',
 
-     # Local
-    'podcasts.apps.PodcastsConfig',]
+    # Local
+    'podcasts.apps.PodcastsConfig', ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',]
+    'django.middleware.clickjacking.XFrameOptionsMiddleware', ]
 
 ROOT_URLCONF = 'main.urls'
 SITE_ID = 1
@@ -69,7 +69,7 @@ TEMPLATES = [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',],},},]
+                'django.contrib.messages.context_processors.messages', ], }, }, ]
 
 WSGI_APPLICATION = 'main.wsgi.application'
 
@@ -83,20 +83,20 @@ DATABASES = {
         'USER': os.getenv('DATABASE_USER'),
         'PASSWORD': os.getenv('DATABASE_PASSWORD'),
         'HOST': 'db',
-        'PORT': 5432,}}
+        'PORT': 5432, }}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',},
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',},
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',},
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator', },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',},]
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator', }, ]
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
@@ -116,11 +116,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRES = [
-    BASE_DIR / 'static',]
+    BASE_DIR / 'static', ]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
-    "django.contrib.staticfiles.finders.AppDirectoriesFinder",]
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder", ]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
@@ -135,7 +135,7 @@ LOGGING = {
     "disable_existing_loggers": False,
     "formatters": {"simple": {"format": "%(levelname)s %(message)s"}},
     "handlers": {"console": {"class": "logging.StreamHandler", "formatter": "simple"}},
-    "root": {"handlers": ["console"], "level": "INFO"},}
+    "root": {"handlers": ["console"], "level": "INFO"}, }
 
 if not DEBUG:
     SECURE_BROWSER_XSS_FILTER = True
