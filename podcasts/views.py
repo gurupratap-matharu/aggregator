c906c5c9f31055e264fa3d79af6575f4fef933c0
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-11-11 13:28:34 +0530

    Add view to process search queries

--- podcasts/views.py
+++ podcasts/views.py
@@ -1,6 +1,7 @@
 import logging
 from typing import Any, Dict
 
+from django.db.models import Q, QuerySet
 from django.views.generic import ListView
 
 from podcasts.models import Episode
@@ -19,3 +20,16 @@ class HomePageView(ListView):
         context = super().get_context_data(**kwargs)
         context["episodes"] = Episode.objects.active().order_by('-featured')
         return context
+
+
+class SearchResultsListView(ListView):
+    model = Episode
+    context_object_name = 'episodes'
+    template_name = 'podcasts/homepage.html'
+    paginage_by = 12
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        return Episode.objects.active().filter(
+            Q(title__icontains=query) | Q(description__icontains=query)
+        )
