74a81a8e7761f2aed26b987b516f988fd654703c
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-11-11 13:28:22 +0530

    Add endpoint for search

--- podcasts/urls.py
+++ podcasts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 
-from podcasts.views import HomePageView
+from podcasts.views import HomePageView, SearchResultsListView
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='homepage'), ]
+    path('', HomePageView.as_view(), name='homepage'),
+    path('search/', SearchResultsListView.as_view(), name='search_results'),
+]
